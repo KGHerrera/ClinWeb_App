a0532f530f64863f7ccf3cb85554308261ebedcf
KGHerrera <bycrashdash@gmail.com>
2023-11-12 12:35:05 -0600

    se agrego el formulario para agregar citas

--- app/src/main/java/com/example/test/CitaActivity.java
+++ app/src/main/java/com/example/test/CitaActivity.java
@@ -1,10 +1,19 @@
 package com.example.test;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +34,33 @@ public class CitaActivity extends AppCompatActivity {
         List<Cita> citaList = getCitas();
         CitaAdapter adapter = new CitaAdapter(citaList);
         recyclerView.setAdapter(adapter);
+
+
+        FloatingActionButton fabAgregar = findViewById(R.id.fabAgregar);
+        fabAgregar.setOnClickListener(view -> mostrarFormulario());
+    }
+
+
+
+    private void mostrarFormulario() {
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View formularioView = inflater.inflate(R.layout.formulario, null);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setView(formularioView)
+                .setTitle("Agregar Cita")
+                .setPositiveButton("Guardar", (dialog, which) -> {
+                    // Manejar clic en el botón "Guardar"
+                    // Puedes obtener datos del formulario utilizando formularioView.findViewById(R.id.xxx)
+                })
+                .setNegativeButton("Cancelar", (dialog, which) -> {
+                    // Manejar clic en el botón "Cancelar" o simplemente cerrar el diálogo
+                    dialog.dismiss();
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
     private List<Cita> getCitas() {
