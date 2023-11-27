5cdb157cfe0229091940625287a84fba53ed47d0
KGHerrera <bycrashdash@gmail.com>
2023-11-27 12:53:05 -0600

    se agrego funcionalidad al la pantalla main o de login

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -6,25 +6,63 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.test.controlador.AnalizadorJSON;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import modelo.Cita;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editUsuario;
+    EditText editContrasena;
+
+    String urlApi;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        urlApi = "http://clinweb.000webhostapp.com/API/";
 
         Button loginButton = findViewById(R.id.loginButton);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        editUsuario = findViewById(R.id.usernameEditText);
+        editContrasena = findViewById(R.id.passwordEditText);
 
-                Intent intent = new Intent(MainActivity.this, CitaActivity.class);
-                startActivity(intent);
-            }
+        loginButton.setOnClickListener(v -> {
+
+            String usuario = editUsuario.getText().toString();
+            String contrasena = editContrasena.getText().toString();
+
+            new Thread(() -> {
+                String url = urlApi + "verificarUsuario.php";
+                String metodo = "POST";
+                AnalizadorJSON analizadorJSON = new AnalizadorJSON();
+                JSONObject jsonObject = analizadorJSON.verificarUsuario(url, metodo, usuario, contrasena);
+
+                try {
+                    boolean exito = jsonObject.getBoolean("exito");
+                    if (exito) {
+                        runOnUiThread(() -> {
+                            Intent intent = new Intent(MainActivity.this, CitaActivity.class);
+                            startActivity(intent);
+                        });
+                    } else {
+                        runOnUiThread(() -> {
+                            Toast.makeText(MainActivity.this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show();
+                        });
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }).start();
         });
     }
 }
